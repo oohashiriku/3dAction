c772f25cdaa0235bb0df42c0172ef41c995767fa
RK <riku.kuu0421@gmail.com>
2022-06-25 16:55:44 +0900

    FreeLookCameraの調整

---
+++ Assets/my script/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
