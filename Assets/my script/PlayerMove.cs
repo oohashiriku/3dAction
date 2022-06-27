91156b548dcff5a70129b85a61267ee1f2aef2f2
RK <riku.kuu0421@gmail.com>
2022-06-27 19:05:55 +0900

    ライト作成

--- Assets/my script/PlayerMove.cs
+++ Assets/my script/PlayerMove.cs
@@ -23,8 +23,7 @@ public class PlayerMove : MonoBehaviour
             //正面に速度を代入
             transform.forward = dir;
         }
-        rb.velocity = dir.normalized * moveSpeed + new Vector3(0f, rb.velocity.y, 0f);
-        //rb.velocity = new Vector3(dir.normalized.x * moveSpeed, rb.velocity.y, dir.normalized.z * moveSpeed);
+            rb.velocity = dir.normalized * moveSpeed + new Vector3(0f, rb.velocity.y, 0f);
     }
     void Update()
     {
@@ -36,12 +35,12 @@ public class PlayerMove : MonoBehaviour
         //入力
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
-        dir = new Vector3(moveX, 0, moveZ);
         //方向ベクトルを取得
+        dir = new Vector3(moveX, 0, moveZ);
+        
     }
     void Jump()
     {
-        //bool jump = ;
         if (Input.GetButtonDown("Fire1"))
         {
             Debug.Log("aaa");
