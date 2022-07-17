2aedd7fd34dca26b110a37c66b9dc4f86b5634d9
RK <riku.kuu0421@gmail.com>
2022-07-17 21:42:40 +0900

    scriptの整理

--- Assets/my script/PlayerAttack.cs
+++ Assets/my script/PlayerAttack.cs
@@ -7,11 +7,13 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] GameObject _comboEffect;
     [SerializeField] GameObject _rushEffect;
     [SerializeField] Transform _rushMuzzle;
-    [SerializeField] Transform _comboMuzzle1;
-    [SerializeField] Transform _comboMuzzle2;
-    [SerializeField] Transform _comboMuzzle3;
+    //[SerializeField] Transform _comboMuzzle1;
+    //[SerializeField] Transform _comboMuzzle2;
+    //[SerializeField] Transform _comboMuzzle3;
+    [SerializeField] Transform[] _comboMuzzle;
     [SerializeField] float _attackSpeed;
     [SerializeField] GameObject _swordEquip;
+    [SerializeField] float _test;
     Animator _anim;
     int _comboCount = 0;
     bool _canCombo = false;
@@ -34,7 +36,6 @@ public class PlayerAttack : MonoBehaviour
         //psコントローラの〇ボタンを押したとき。かつ、剣を構えてるときに以下を実行する。
         if (Input.GetButtonDown("Fire3") && _swordEquip.activeSelf && !_canCombo)
         {
-            _canAttack = true;
             //attackモーションスタート
             StartCoroutine(IsAttack(_comboCount % 3 + 1));//コンボ数の上限を3にする。
             //コンボ数をカウント
@@ -46,6 +47,7 @@ public class PlayerAttack : MonoBehaviour
     {
 
         yield return new WaitForSeconds(0.1f);//0.1秒待つ
+        _canAttack = true;
         _anim.SetBool($"isAttack{i}", true);
         yield return new WaitForSeconds(0.2f);//0.2秒待つ
         _anim.SetBool($"isAttack{i}", false);
@@ -55,6 +57,7 @@ public class PlayerAttack : MonoBehaviour
     {
         if(Input.GetButtonDown("rush") && _swordEquip.activeSelf && !_canRush)
         {
+            _canAttack = true;
             _anim.SetTrigger("isTuki");
             StartCoroutine(RushInterval());
         }
@@ -73,30 +76,29 @@ public class PlayerAttack : MonoBehaviour
         _instance.transform.parent = transform;
         _rb.AddForce(transform.forward * _attackSpeed, ForceMode.Impulse);
     }
-    void ComboEffect1()
+    void ComboEffect1(int i)
     {
         GameObject _instance = Instantiate(_comboEffect);
-        _instance.transform.position = _comboMuzzle1.transform.position;
-        _instance.transform.rotation = _comboMuzzle1.transform.rotation;
+        _instance.transform.position = _comboMuzzle[i].transform.position;
+        _instance.transform.rotation = _comboMuzzle[i].transform.rotation;
         _instance.transform.parent = transform;
     }
-    void ComboEffect2()
-    {
-        GameObject _instance = Instantiate(_comboEffect);
-        _instance.transform.position = _comboMuzzle2.transform.position;
-        _instance.transform.rotation = _comboMuzzle2.transform.rotation;
-        _instance.transform.parent = transform;
-    }
-    void ComboEffect3()
-    {
-        GameObject _instance = Instantiate(_comboEffect);
-        _instance.transform.position = _comboMuzzle3.transform.position;
-        _instance.transform.rotation = _comboMuzzle3.transform.rotation;
-        _instance.transform.parent = transform;
-    }
+    //void ComboEffect2()
+    //{
+    //    GameObject _instance = Instantiate(_comboEffect);
+    //    _instance.transform.position = _comboMuzzle2.transform.position;
+    //    _instance.transform.rotation = _comboMuzzle2.transform.rotation;
+    //    _instance.transform.parent = transform;
+    //}
+    //void ComboEffect3()
+    //{
+    //    GameObject _instance = Instantiate(_comboEffect);
+    //    _instance.transform.position = _comboMuzzle3.transform.position;
+    //    _instance.transform.rotation = _comboMuzzle3.transform.rotation;
+    //    _instance.transform.parent = transform;
+    //}
     void CanComboMove()
     {
         _canAttack = false;
     }
-
 }
